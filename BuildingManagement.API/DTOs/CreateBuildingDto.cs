0c57e169cf1311819ff6e9fdaadea042b1813a73
Eugene Elmore <melmore@Eugenes-MacBook-Pro.local>
2026-06-27 12:32:54 -0400

    feat: add DataAnnotations validation for building DTO "

--- BuildingManagement.API/DTOs/CreateBuildingDto.cs
+++ BuildingManagement.API/DTOs/CreateBuildingDto.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BuildingManagement.API.DTOs;
 
 public class CreateBuildingDto
-    {
+    {   
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(200)]
         public string Address { get; set; } = string.Empty;
 
+        [Range(1, 10000)]
         public int NumberOfUnits { get; set; }
     }
